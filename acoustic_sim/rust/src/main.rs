89203bac18db4323a356e3c34a96957674f362dc
Quinn Ferbers <gqferbs@gmail.com>
2024-01-23 23:06:36 -0800

    first crack at multithreaded rust

--- acoustic_sim/rust/src/main.rs
+++ acoustic_sim/rust/src/main.rs
@@ -1,17 +1,27 @@
-mod hat;
+#![allow(unused_imports)]
+// mod hat;
+mod hat_mt;
 
 use num::complex::Complex;
 use std::f32::consts::PI;
 use std::time::Instant;
 
+use std::sync::Arc;
+
+// fn main() {
+//     let control_points = vec![hat_mt::Point {
+//         x: 0.05,
+//         y: 0.05,
+//         z: 0.05,
+//     }];
+//
+//     let phases = hat_mt::run_hat(Arc::new(control_points), 16.0, 0.1);
+//     println!("ran");
+//     println!("{:?}", phases);
+// }
+
 fn main() {
     // setup test case
-    let control_points = vec![hat::Point {
-        x: 0.05,
-        y: 0.05,
-        z: 0.05,
-    }];
-
     let t_sep = 0.01;
     let num_t = 100;
     let ts: Vec<f32> = (0..num_t).map(|t| t as f32 * t_sep).collect();
@@ -28,12 +38,12 @@ fn main() {
         .map(|t| 0.02 * (2.0 * PI * t).sin() + 0.05)
         .collect();
 
-    let cps: Vec<Vec<hat::Point>> = xs
+    let cps: Vec<Vec<hat_mt::Point>> = xs
         .iter()
         .zip(ys.iter())
         .zip(zs.iter())
         .map(|((x, y), z)| {
-            vec![hat::Point {
+            vec![hat_mt::Point {
                 x: *x,
                 y: *y,
                 z: *z,
@@ -46,10 +56,60 @@ fn main() {
     // benchmark
     let now = Instant::now();
     for control_points in cps {
-        phases.push(hat::run_hat(&control_points, 16.0, 0.1));
+        phases.push(hat_mt::run_hat(Arc::new(control_points), 16.0, 0.1));
     }
     let time = now.elapsed();
 
     println!("Benchmark took {} seconds.", time.as_secs_f32());
     println!("frames per second: {}", 100.0 / time.as_secs_f32());
 }
+
+// fn main() {
+//     // setup test case
+//     let control_points = vec![hat::Point {
+//         x: 0.05,
+//         y: 0.05,
+//         z: 0.05,
+//     }];
+//
+//     let t_sep = 0.01;
+//     let num_t = 100;
+//     let ts: Vec<f32> = (0..num_t).map(|t| t as f32 * t_sep).collect();
+//     let xs: Vec<f32> = ts
+//         .iter()
+//         .map(|t| 0.02 * (2.0 * PI * t).sin() + 0.05)
+//         .collect();
+//     let ys: Vec<f32> = ts
+//         .iter()
+//         .map(|t| 0.02 * (2.0 * PI * t).sin() + 0.05)
+//         .collect();
+//     let zs: Vec<f32> = ts
+//         .iter()
+//         .map(|t| 0.02 * (2.0 * PI * t).sin() + 0.05)
+//         .collect();
+//
+//     let cps: Vec<Vec<hat::Point>> = xs
+//         .iter()
+//         .zip(ys.iter())
+//         .zip(zs.iter())
+//         .map(|((x, y), z)| {
+//             vec![hat::Point {
+//                 x: *x,
+//                 y: *y,
+//                 z: *z,
+//             }]
+//         })
+//         .collect();
+//
+//     let mut phases: Vec<Vec<Complex<f32>>> = vec![];
+//
+//     // benchmark
+//     let now = Instant::now();
+//     for control_points in cps {
+//         phases.push(hat::run_hat(&control_points, 16.0, 0.1));
+//     }
+//     let time = now.elapsed();
+//
+//     println!("Benchmark took {} seconds.", time.as_secs_f32());
+//     println!("frames per second: {}", 100.0 / time.as_secs_f32());
+// }
