6f1b7059decd6fb2d8a833c6158075e640b8b805
Kevin Lin <kevinlinxc@gmail.com>
2024-03-01 21:51:40 -0800

    hand working well. Run hand.py, rust_to_blender.py in blender, and redis and it works.

--- blender_sim/rust_to_blender.py
+++ blender_sim/rust_to_blender.py
@@ -17,10 +17,10 @@ bpy.ops.object.delete()
 # Define the dimensions of the array
 num_rows = 10
 num_columns = 10
-spacing = 10/1000  # Adjust this to control the spacing between cylinders
+spacing = 10 / 1000  # Adjust this to control the spacing between cylinders
 
 # Create a cylinder template
-bpy.ops.mesh.primitive_cylinder_add(vertices=32, radius=5/1000, depth=7/1000, location=(0, 0, 0))
+bpy.ops.mesh.primitive_cylinder_add(vertices=32, radius=5 / 1000, depth=7 / 1000, location=(0, 0, 0))
 cylinder_template = bpy.context.object
 
 # Create the array of cylinders
@@ -35,9 +35,9 @@ for row in range(num_rows):
 # Select and link the template to the scene
 bpy.context.collection.objects.unlink(cylinder_template)
 bpy.data.objects.remove(cylinder_template)
-#bpy.ops.mesh.primitive_uv_sphere_add(radius=0.01, location=(0, 0, 1))
-#ball = bpy.context.object
-#ball.name = 'sphere'
+# bpy.ops.mesh.primitive_uv_sphere_add(radius=0.01, location=(0, 0, 1))
+# ball = bpy.context.object
+# ball.name = 'sphere'
 
 
 # This function can safely be called in another thread.
@@ -48,28 +48,30 @@ r = redis.StrictRedis(host='localhost', port=6379, db=0, decode_responses=True)
 # Create a pubsub instance and subscribe to the 'positions' channel
 pubsub = r.pubsub()
 pubsub.subscribe('positions')
-#context = zmq.Context()
-#socket = context.socket(zmq.SUB)
-#socket.connect("tcp://localhost:5498")
+
+
+# context = zmq.Context()
+# socket = context.socket(zmq.SUB)
+# socket.connect("tcp://localhost:5498")
 
 # Subscribe to all messages
-#socket.setsockopt_string(zmq.SUBSCRIBE, '')
-    
+# socket.setsockopt_string(zmq.SUBSCRIBE, '')
+
 def read_to_queue():
     for message in pubsub.listen():
         if message['type'] == 'message':
             positions_list = json.loads(message['data'])
-            positions_list = ast.literal_eval(positions_list)
+            if type(positions_list) == str:  # idk why rust sends a str but python sends a list
+                positions_list = ast.literal_eval(positions_list)
             location_queue.put(positions_list)
 
 
 def use_locations_from_queue():
     while not location_queue.empty():
         locations = location_queue.get()
-        print(locations)
         for index, location in enumerate(locations):
             name = "sphere_" + str(index)
-            if obj:= bpy.context.scene.objects.get(name):
+            if obj := bpy.context.scene.objects.get(name):
                 obj.location = location
             else:
                 bpy.ops.mesh.primitive_uv_sphere_add(radius=0.005, location=location)
--- hand_control/hand.py
+++ hand_control/hand.py
@@ -1,16 +1,20 @@
-import cv2
-
-import mediapipe as mp
-from mediapipe import solutions
 import math
-# For drawing landmarks on an image, we will use the following function:
-
 import os
 import sys
 import time
+import json
+
+
+import cv2
+import mediapipe as mp
+from mediapipe import solutions
 from mediapipe.framework.formats import landmark_pb2
-import numpy as np
 from mediapipe.tasks.python import vision
+import redis
+from mediapipe.python.solutions.hands import HandLandmark
+import numpy as np
+# For drawing landmarks on an image, we will use the following function:
+
 
 MARGIN = 10  # pixels
 FONT_SIZE = 1
@@ -24,12 +28,31 @@ GestureRecognizerResult = mp.tasks.vision.GestureRecognizerResult
 VisionRunningMode = mp.tasks.vision.RunningMode
 mp_drawing = mp.solutions.drawing_utils
 global_img, global_results = None, None
+global_y_scale = None
+pinching = False
+redis_inst = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 model_path = "gesture_recognizer.task"
 if not os.path.exists(model_path):
     print("Go download pose_landmarker_full from "
           "https://developers.google.com/mediapipe/solutions/vision/gesture_recognizer#models")
     sys.exit(1)
+
+
+def get_board_position_from_hand_positions(x_normalised, y_normalised, pinky_to_index_distance):
+    # x position on picture maps to x position on board, y on picture maps to z on board, pinky to index distance
+    # scale maps to y on board
+    board_x_max = 0.1
+    board_y_max = 0.1
+    board_z_max = 0.1
+    # flip x normalized and y normalized because camera is flipped and y starts at 0 at top
+    x_pos = (1-x_normalised) * board_x_max
+    global global_y_scale
+    # if pinky_to_index_distance is same as global_y_scale we should be at half of board_y_max.
+    # Closer(larger ptdi)is larger y,further (smaller ptdi) is smaller y
+    y_pos = (pinky_to_index_distance / global_y_scale) * (board_y_max / 2)
+    z_pos = (1-y_normalised) * board_z_max
+    return x_pos, y_pos, z_pos
 
 
 def draw_landmarks_on_image(rgb_image, detection_result):
@@ -71,34 +94,65 @@ def draw_landmarks_on_image(rgb_image, detection_result):
 
 # Create a pose landmarker instance with the live stream mode:
 def print_result(result: GestureRecognizerResult, output_image: mp.Image, timestamp_ms: int):
-    global global_img
+    global global_img, pinching
     # print('hand landmarker result: {}'.format(result))
     # convert image to cv2 format
     image = output_image.numpy_view()
-    image = image.copy()
-    # make image from numpy array
-    lml = []
-    xl = []
-    yl = []
+    image = image.copy()  # need to copu to edit
+    img = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    # add landmarks
+    img = draw_landmarks_on_image(img, result)
+
     if len(result.hand_landmarks) == 0:
-        global_img = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        img = cv2.flip(img, 1)
+        global_img = img
         return
-    for id, lm in enumerate(result.hand_landmarks[0]):
-        h, w, _ = image.shape
-        xc, yc = int(lm.x * w), int(lm.y * h)
-        lml.append([id, xc, yc])
-        xl.append(xc)
-        yl.append(yc)
-    global_img = cv2.cvtColor(global_img, cv2.COLOR_RGB2BGR)
-    x1, y1 = lml[4][1], lml[4][2]
-    x2, y2 = lml[8][1], lml[8][2]
-    cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
-    cv2.circle(image, (x1, y1), 10, (255, 0, 128), cv2.FILLED)
-    cv2.circle(image, (x2, y2), 10, (255, 0, 128), cv2.FILLED)
-    cv2.line(image, (x1, y1), (x2, y2), (255, 0, 128), 3)
-    distance = math.hypot(x2 - x1, y2 - y1)
-    img = draw_landmarks_on_image(image, result)
-    cv2.putText(img, str(int(distance)), (cx + 30, cy), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 128), 3)
+    h, w, _ = image.shape
+
+    # info:
+    # use thumb and index tip distance to see if pinching. If pinching, use centroid of pinch as the z and x axis,
+    # and use the scale of the pinky to index finger when the pinch happens as the y axis while pinched
+    thumb_tip_x_norm = result.hand_landmarks[0][HandLandmark.THUMB_TIP].x
+    thumb_tip_y_norm = result.hand_landmarks[0][HandLandmark.THUMB_TIP].y
+    index_tip_x_norm = result.hand_landmarks[0][HandLandmark.INDEX_FINGER_TIP].x
+    index_tip_y_norm = result.hand_landmarks[0][HandLandmark.INDEX_FINGER_TIP].y
+    index_knuckle_x_norm = result.hand_landmarks[0][HandLandmark.INDEX_FINGER_DIP].x
+    index_knuckle_y_norm = result.hand_landmarks[0][HandLandmark.INDEX_FINGER_DIP].y
+    pinky_tip_x_norm = result.hand_landmarks[0][HandLandmark.PINKY_TIP].x
+    pinky_tip_y_norm = result.hand_landmarks[0][HandLandmark.PINKY_TIP].y
+
+    x_thumb_tip, y_thumb_tip = int(thumb_tip_x_norm * w), int(thumb_tip_y_norm * h)
+    x_index_tip, y_index_tip = int(index_tip_x_norm * w), int(index_tip_y_norm * h)
+    x_index_knuckle, y_index_knuckle = int(index_knuckle_x_norm * w), int(index_knuckle_y_norm * h)
+    x_pinky_tip, y_pinky_tip = int(pinky_tip_x_norm * w), int(pinky_tip_y_norm * h)
+    cx, cy = (x_thumb_tip + x_index_tip) // 2, (y_thumb_tip + y_index_tip) // 2
+    cv2.circle(img, (x_thumb_tip, y_thumb_tip), 10, (255, 0, 128), cv2.FILLED)
+    cv2.circle(img, (x_index_tip, y_index_tip), 10, (255, 0, 128), cv2.FILLED)
+    # use index tip to knuckle as a reference
+    index_one_knuckle_distance = math.hypot(x_index_knuckle - x_index_tip, y_index_knuckle - y_index_tip)
+    pinch_distance = math.hypot(x_index_tip - x_thumb_tip, y_index_tip - y_thumb_tip)
+
+    pinky_to_index_distance = math.hypot(x_index_tip - x_pinky_tip, y_index_tip - y_pinky_tip)
+    if pinch_distance > index_one_knuckle_distance * 2:
+        pinching = False
+        cv2.line(img, (x_thumb_tip, y_thumb_tip), (x_index_tip, y_index_tip), (255, 0, 128), 3)
+        cv2.putText(img, str(int(pinch_distance)), (cx + 30, cy), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 128), 3)
+    else:
+        if not pinching:
+            global global_y_scale
+            global_y_scale = pinky_to_index_distance
+        pinching = True
+        cv2.circle(img, (cx, cy), int(pinch_distance), (255, 255, 128), 2)
+        x_pos, y_pos, z_pos = get_board_position_from_hand_positions(cx/w, cy/h, pinky_to_index_distance)
+        position = [x_pos, y_pos, z_pos]
+        positions = [position]  # in our system we send lists of positions for multiple trap compatibility
+        msg_packed = repr(positions).encode('utf-8')
+        redis_inst.publish('positions', msg_packed)
+        # add coordinates to frame
+        cv2.putText(img, f"({x_pos:.2f}, {y_pos:.2f}, {z_pos:.2f})", (10, 30), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 128), 3)
+        # send to blender over redis
+    # flip across y axis
+    img = cv2.flip(img, 1)
     global_img = img
 
 
@@ -109,7 +163,7 @@ options = GestureRecognizerOptions(
     num_hands=2)
 
 with vision.GestureRecognizer.create_from_options(options) as landmarker:
-    cap = cv2.VideoCapture(1)
+    cap = cv2.VideoCapture(0)
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
