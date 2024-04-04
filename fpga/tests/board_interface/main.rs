649233970d4eec1d40d998e7cd91e1b904398876
Kevin Lin <kevinlinxc@gmail.com>
2024-04-03 20:00:03 -0700

    add printout to fpga test

--- fpga/tests/board_interface/main.rs
+++ fpga/tests/board_interface/main.rs
@@ -1,6 +1,14 @@
 use rev1::board::Board;
+use libftd2xx::{list_devices, DeviceInfo};
+
 
 fn main() {
+
+    println!("Hello, world!");
+    let devices: Vec<DeviceInfo> = list_devices().unwrap();
+    for device in devices {
+        println!("device properties: {:?}", device);
+    }
     match Board::new() {
         Ok(mut board) => {
             let phases: Vec<f32> = vec![0.00, 0.79, 1.57, 2.36, 3.14, 3.97, 4.71, 5.49];
